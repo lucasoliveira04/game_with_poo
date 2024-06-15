c0e6b3938266fb23bc745b29ee0e176ae8d40967
lucasoliveira04 <camposdlucasoli@gmail.com>
2024-06-14 21:31:41 -0300

    Logica de ataque finalizada

--- src/ConfigurationActions/Attack/Attack.java
+++ src/ConfigurationActions/Attack/Attack.java
@@ -8,14 +8,29 @@ import java.util.logging.Logger;
 public class Attack implements ActionsInterface {
     private static final Logger LOGGER = Logger.getAnonymousLogger();
     private PersonagemBase personagemBase;
+    private int ataquesPadraoRealizados;
+    private boolean especialDesbloqueado;
 
     public Attack(PersonagemBase personagemBase) {
         this.personagemBase = personagemBase;
+        this.ataquesPadraoRealizados = 0;
+        this.especialDesbloqueado = false;
     }
 
     @Override
     public void action(String optionSkill) {
-        attackDefault(optionSkill);
+        if (especialDesbloqueado && isSpecialSkill(optionSkill)) {
+            attackSpecial(optionSkill);
+            reiniciarAtaquePadrao();
+        } else {
+            attackDefault(optionSkill);
+            ataquesPadraoRealizados++;
+
+            if (ataquesPadraoRealizados >= 3) {
+                especialDesbloqueado = true;
+                System.out.println("Você desbloqueou o ataque especial: " + personagemBase.getHabilidadeSpecial());
+            }
+        }
     }
 
     private void attackDefault(String habilidade) {
@@ -27,5 +42,34 @@ public class Attack implements ActionsInterface {
         } else {
             System.out.println("Habilidade " + habilidade + " não encontrada.");
         }
+    }
+
+    private void attackSpecial(String habilidade) {
+        habilidade = habilidade.toLowerCase();
+        if (isSpecialSkill(habilidade)) {
+            Double dano = personagemBase.getDanoDeCadaHabilidade().get(habilidade);
+
+            if (dano != null) {
+                System.out.println("Ataque especial com " + habilidade + " causou " + dano + " de dano.");
+            } else {
+                System.out.println("Habilidade " + habilidade + " não encontrada.");
+            }
+        } else {
+            System.out.println("Habilidade " + habilidade + " não é o ataque especial.");
+        }
+    }
+
+    public boolean isEspecialDesbloqueado(){
+        return especialDesbloqueado;
+    }
+
+    public void reiniciarAtaquePadrao(){
+        ataquesPadraoRealizados = 0;
+        especialDesbloqueado = false;
+        System.out.println("Contador de ataques padrão reiniciado.");
+    }
+
+    public boolean isSpecialSkill(String habilidade){
+        return habilidade.equalsIgnoreCase(personagemBase.getHabilidadeSpecial());
     }
 }
--- src/Personagem/PersonagemBase.java
+++ src/Personagem/PersonagemBase.java
@@ -1,6 +1,7 @@
 package Personagem;
 
 import ConfigurationActions.MapActions.SkillsDefault;
+import ConfigurationActions.MapActions.SkillsSpecial;
 import lombok.*;
 
 import java.util.HashMap;
@@ -15,20 +16,28 @@ public class PersonagemBase {
     private String habilidade1;
     private String habilidade2;
     private String habilidade3;
+    private String habilidadeSpecial;
     private double vida = 100;
     private Map<String, Double> danoDeCadaHabilidade = new HashMap<>();
+    private double danoTotalCausado;
 
     // Construtor que inicializa danoDeCadaHabilidade com valores padrão
-    public PersonagemBase(String nome, String habilidade1, String habilidade2, String habilidade3) {
+    public PersonagemBase(String nome, String habilidade1, String habilidade2, String habilidade3, String habilidadeSpecial) {
         this.nome = nome;
         this.habilidade1 = habilidade1;
         this.habilidade2 = habilidade2;
         this.habilidade3 = habilidade3;
+        this.habilidadeSpecial = habilidadeSpecial;
 
         Map<String, Double> skillsDefault = SkillsDefault.getSkillsDefault();
+        Map<String, Double> skillsSpecial = SkillsSpecial.getSkillsSpecial();
+
         danoDeCadaHabilidade.put(habilidade1, skillsDefault.getOrDefault(habilidade1, 0.0));
         danoDeCadaHabilidade.put(habilidade2, skillsDefault.getOrDefault(habilidade2, 0.0));
         danoDeCadaHabilidade.put(habilidade3, skillsDefault.getOrDefault(habilidade3, 0.0));
+        danoDeCadaHabilidade.put(habilidadeSpecial, skillsSpecial.getOrDefault(habilidadeSpecial, 0.0));
+
+        this.danoTotalCausado = 0.0;
     }
 
     public String getInformations() {
@@ -37,7 +46,29 @@ public class PersonagemBase {
         sb.append("Habilidade 1: ").append(habilidade1).append(" (Dano: ").append(danoDeCadaHabilidade.get(habilidade1)).append(")\n");
         sb.append("Habilidade 2: ").append(habilidade2).append(" (Dano: ").append(danoDeCadaHabilidade.get(habilidade2)).append(")\n");
         sb.append("Habilidade 3: ").append(habilidade3).append(" (Dano: ").append(danoDeCadaHabilidade.get(habilidade3)).append(")\n");
+        sb.append("Habilidade Special: ").append(habilidadeSpecial).append("\n");
         return sb.toString();
+    }
+
+    public void incrementarDanoCausado(String habilidade) {
+        double dano = danoDeCadaHabilidade.getOrDefault(habilidade, 0.0);
+        danoTotalCausado += dano;
+    }
+
+    public void zerarDanoCausado() {
+        danoTotalCausado = 0.0;
+    }
+
+    public double getDanoTotalCausado() {
+        return danoTotalCausado;
+    }
+
+    public void setDanoTotalCausado(double danoTotalCausado) {
+        this.danoTotalCausado = danoTotalCausado;
+    }
+
+    public void adicionarDanoCausado(double dano){
+        this.danoTotalCausado += dano;
     }
 
     public String getNome() {
@@ -94,5 +125,13 @@ public class PersonagemBase {
 
     public void setDanoDeCadaHabilidade(Map<String, Double> danoDeCadaHabilidade) {
         this.danoDeCadaHabilidade = danoDeCadaHabilidade;
+    }
+
+    public String getHabilidadeSpecial() {
+        return habilidadeSpecial;
+    }
+
+    public void setHabilidadeSpecial(String habilidadeSpecial) {
+        this.habilidadeSpecial = habilidadeSpecial;
     }
 }
